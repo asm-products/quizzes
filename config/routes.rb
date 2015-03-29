d8c8ed413c5423923e8534ca6d2620f63913d4ae
Taran <tns12@scasd.org>
2015-03-29 18:21:14 -0400

    Devise and bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  root 'pages/home'
+  root 'pages#home'
   devise_for :teachers
+  
+  as :teacher do 
+  	get "login" => "devise/sessions#new"
+  	post "login" => "devise/sessions#create"
+  	get "logout" => "devise/sessions#destroy"
+
+  	get "signup" => "devise/registrations#new"
+  	post "signup" => "devise/registrations#create"
+  	put "signup" => "devise/registrations#update"
+  	delete "signup" => "devise/registrations#destroy"
+
+  	get "settings" => "devise/registrations#edit"
+
+  	get "forgot_password" => "devise/passwords#new"
+  	get "change_password" => "devise/passwords#edit"
+end
 end
